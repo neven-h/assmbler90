d7f4b81f350818a6adb0bb2ab40c0901babcf160
ronia <ronialon2008@gmail.com>
2021-08-13 16:32:01 +0300

    /

---
+++ cmake-build-debug/CMakeFiles/RoniProject.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/ronia/CLionProjects/assmbler90/EntryAndExtern.c" "C:/Users/ronia/CLionProjects/assmbler90/cmake-build-debug/CMakeFiles/RoniProject.dir/EntryAndExtern.c.obj"
+  "C:/Users/ronia/CLionProjects/assmbler90/WordList.c" "C:/Users/ronia/CLionProjects/assmbler90/cmake-build-debug/CMakeFiles/RoniProject.dir/WordList.c.obj"
+  "C:/Users/ronia/CLionProjects/assmbler90/directiveAnalyzed.c" "C:/Users/ronia/CLionProjects/assmbler90/cmake-build-debug/CMakeFiles/RoniProject.dir/directiveAnalyzed.c.obj"
+  "C:/Users/ronia/CLionProjects/assmbler90/firstPass.c" "C:/Users/ronia/CLionProjects/assmbler90/cmake-build-debug/CMakeFiles/RoniProject.dir/firstPass.c.obj"
+  "C:/Users/ronia/CLionProjects/assmbler90/inputAnalyzed.c" "C:/Users/ronia/CLionProjects/assmbler90/cmake-build-debug/CMakeFiles/RoniProject.dir/inputAnalyzed.c.obj"
+  "C:/Users/ronia/CLionProjects/assmbler90/instructionAnalyzed.c" "C:/Users/ronia/CLionProjects/assmbler90/cmake-build-debug/CMakeFiles/RoniProject.dir/instructionAnalyzed.c.obj"
+  "C:/Users/ronia/CLionProjects/assmbler90/lableList.c" "C:/Users/ronia/CLionProjects/assmbler90/cmake-build-debug/CMakeFiles/RoniProject.dir/lableList.c.obj"
+  "C:/Users/ronia/CLionProjects/assmbler90/main.c" "C:/Users/ronia/CLionProjects/assmbler90/cmake-build-debug/CMakeFiles/RoniProject.dir/main.c.obj"
+  "C:/Users/ronia/CLionProjects/assmbler90/printErrors.c" "C:/Users/ronia/CLionProjects/assmbler90/cmake-build-debug/CMakeFiles/RoniProject.dir/printErrors.c.obj"
+  "C:/Users/ronia/CLionProjects/assmbler90/secondPass.c" "C:/Users/ronia/CLionProjects/assmbler90/cmake-build-debug/CMakeFiles/RoniProject.dir/secondPass.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RoniProject.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RoniProject.dir/EntryAndExtern.c.obj"
+  "CMakeFiles/RoniProject.dir/WordList.c.obj"
+  "CMakeFiles/RoniProject.dir/directiveAnalyzed.c.obj"
+  "CMakeFiles/RoniProject.dir/firstPass.c.obj"
+  "CMakeFiles/RoniProject.dir/inputAnalyzed.c.obj"
+  "CMakeFiles/RoniProject.dir/instructionAnalyzed.c.obj"
+  "CMakeFiles/RoniProject.dir/lableList.c.obj"
+  "CMakeFiles/RoniProject.dir/main.c.obj"
+  "CMakeFiles/RoniProject.dir/printErrors.c.obj"
+  "CMakeFiles/RoniProject.dir/secondPass.c.obj"
+  "RoniProject.exe"
+  "RoniProject.exe.manifest"
+  "RoniProject.pdb"
+  "libRoniProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RoniProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
